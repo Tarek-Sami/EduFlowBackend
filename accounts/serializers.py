8f3c75dbfe957b9e282f03465510effb503f3639
Tarek Sami <tareek.sami230@gmail.com>
2026-05-13 00:00:40 +0300

    setting fix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,6 +5,9 @@ from django.contrib.auth import get_user_model
 
 from .models import User
 
+UserModel = get_user_model()
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -23,41 +26,57 @@ class RegisterSerializer(serializers.ModelSerializer):
             "last_name",
         ]
 
-    def validate_username(self, value):
-        if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError(
-                "A user with this username already exists."
-            )
-        return value
-
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError(
-                "A user with this email already exists."
-            )
-        return value
-
-    def validate_phone(self, value):
-        # Ignore empty phone
-        if value in [None, ""]:
-            return None
-
-        if User.objects.filter(phone=value).exists():
-            raise serializers.ValidationError(
-                "A user with this phone number already exists."
-            )
-        return value
-
     def create(self, validated_data):
-        # Convert empty strings to None
-        for field in ["phone", "department", "gender"]:
-            if validated_data.get(field) == "":
-                validated_data[field] = None
-
         password = validated_data.pop("password")
-
         user = User(**validated_data)
         user.set_password(password)
         user.save()
-
         return user
+
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = "email"
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        try:
+            user = UserModel.objects.get(email=email)
+        except UserModel.DoesNotExist:
+            raise AuthenticationFailed("Invalid email or password")
+
+        if not user.check_password(password):
+            raise AuthenticationFailed("Invalid email or password")
+
+        if not user.is_active:
+            raise AuthenticationFailed("User account is disabled")
+
+        refresh = self.get_token(user)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
+
+class CurrentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "role",
+            "department",
+            "phone",
+            "gender",
+            "first_name",
+            "last_name",
+        ]
+        read_only_fields = [
+            "id",
+            "username",
+            "email",
+            "role",
+        ]
