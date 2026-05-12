dffaf0b60ff58b7ff97a76e0a1d9a7e46f391433
Tarek Sami <tareek.sami230@gmail.com>
2026-05-12 17:08:17 +0300

    title added

--- tasks/models.py
+++ tasks/models.py
@@ -49,6 +49,7 @@ class Task(models.Model):
     blank=True,
 )
     progress = models.IntegerField(default=0)
+    title = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.headline
